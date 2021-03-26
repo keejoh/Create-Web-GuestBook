9e3999e2fafdf3ebe878c22e38894c33e9c73344
keejoh <39178056+keejoh@users.noreply.github.com>
2021-03-26 12:59:32 -0400

    Delete login.php

--- login.php
+++
@@ -1,5 +0,0 @@
-<?php // login.php
-$dbHost = 'localhost';//use localhost on AMPPS
-$dbUser = 'root'; //use root on AMPPS
-$dbPassword = 'mysql';//use empty string on AMPPS or your password if you have one
-$dbName = 'Exam1P2';//use your database name on AMPPS
